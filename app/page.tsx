e2fa11bb0eceedf5ff5e082c7565c87d4beefc39
Jitendra Nirnejak <jeetnirnejak@gmail.com>
2025-12-30 18:47:06 +0530

    metadata updated, unused imports removed

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,13 @@
 import * as React from "react"
 
-import * as motion from "motion/react-client"
-
-import { BASE_TRANSITION } from "@/utils/animation"
 import { getMetadata } from "@/utils/metadata"
+
 import Inbox from "@/components/Inbox"
 
 export const metadata = getMetadata({
   path: "/",
-  title: "Next.js App",
-  description: "Next.js TypeScript SaaS Starter",
+  title: "Inbox",
+  description: "Inbox of Email App",
 })
 
 const Home: React.FC = () => {
